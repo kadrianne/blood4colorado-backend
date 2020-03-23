35af65db6f716d733ac05d5b7a04b796ed56d41a
kadrianne <kristine.a.du@gmail.com>
2020-03-23 12:58:39 -0600

    Create index/show actions/routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :locations
+  resources :locations, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
